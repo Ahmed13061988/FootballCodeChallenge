e6bc9576393ac70f76745ca9107f202d14a34afa
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-04 17:08:54 -0600

    cleaning the code

--- script.js
+++ script.js
@@ -39,31 +39,13 @@ const game = {
   },
 };
 
-// const players1 = game.players[0];
-// const players2 = game.players[1];
-
 const [players1, players2] = game.players;
-
-// let gkTeam1 = "";
-// let fieldPlayersTeam1 = [];
-
-// for (let i = 0; i < players1.length; i++) {
-//   if (i === 0) {
-//     gkTeam1 = players1[i];
-//   } else {
-//     fieldPlayersTeam1.push(players1[i]);
-//   }
-// }
 
 const [gk, ...filedPlayers] = players1;
 
 const allPlayers = [...players1, ...players2];
 
 const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
-
-// const team1 = game.odds.team1;
-// const team2 = game.odds.team2;
-// const draw = game.odds.x;
 
 const {
   odds: { team1, x: draw, team2 },
