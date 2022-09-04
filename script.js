d23a30c04e45962e0ddc1922fc80a47f18f05e23
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-04 16:56:55 -0600

    refactoring the code

--- script.js
+++ script.js
@@ -39,27 +39,35 @@ const game = {
   },
 };
 
-const players1 = game.players[0];
-const players2 = game.players[1];
+// const players1 = game.players[0];
+// const players2 = game.players[1];
 
-let gkTeam1 = "";
-let fieldPlayersTeam1 = [];
+const [players1, players2] = game.players;
 
-for (let i = 0; i < players1.length; i++) {
-  if (i === 0) {
-    gkTeam1 = players1[i];
-  } else {
-    fieldPlayersTeam1.push(players1[i]);
-  }
-}
+// let gkTeam1 = "";
+// let fieldPlayersTeam1 = [];
+
+// for (let i = 0; i < players1.length; i++) {
+//   if (i === 0) {
+//     gkTeam1 = players1[i];
+//   } else {
+//     fieldPlayersTeam1.push(players1[i]);
+//   }
+// }
+
+const [gk, ...filedPlayers] = players1;
 
 const allPlayers = [...players1, ...players2];
 
 const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
 
-const team1 = game.odds.team1;
-const team2 = game.odds.team2;
-const draw = game.odds.x;
+// const team1 = game.odds.team1;
+// const team2 = game.odds.team2;
+// const draw = game.odds.x;
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
 
 function printGoals(...playerNames) {
   console.log(`${playerNames}, they scored ${playerNames.length} goals`);
@@ -68,3 +76,5 @@ function printGoals(...playerNames) {
 const winner = game.team1 < game.team2 || game.team1 > game.team2;
 
 printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+
+printGoals(game.scored);
